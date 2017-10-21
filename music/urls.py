3bbc0d3d1c63db0fa5e9827b23a84f3fbbb65562
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2017-10-21 19:04:27 +0300

    Add generic views and forms

--- music/urls.py
+++ music/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
 
     # /music/<album_id>/
     # The brackets group the characters string pattern so that it isn't read as separate items
-    url(r'^(?P<id>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
+
+    # /music/album/add/
+    url(r'^album/add/$', views.AlbumCreate.as_view(), name="create_album"),
 ]
